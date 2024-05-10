5ad818ceff186b4eeda0224c73f4529002c13787
nssuplig-su <nssuplig@syr.edu>
2024-05-10 15:29:24 -0400

    second video- spotify api

---
+++ app/api/artists/route.js
@@ -0,0 +1,40 @@
+import { getAccessToken } from "../../../lib/spotify";
+
+export async function GET() {
+  const token = await getAccessToken();
+
+  //const taylorSwift = "06HL4z0CvFAxyc27GXpf02";
+  const artists = [
+    {
+      name: "Taylor Swift",
+      spotify_id: "06HL4z0CvFAxyc27GXpf02",
+    },
+    {
+      name: "Beyoncé",
+      spotify_id: "6vWDO969PvNqNYHIOW5v0m",
+    },
+    {
+      name: "Ariana Grande",
+      spotify_id: "66CXWjxzNUsdJxJ2JdwvnR",
+    },
+  ];
+
+  // Extract the artist IDs
+  const artistsIds = artists.map((artist) => artist.spotify_id);
+
+  // Convert the artist IDs to a string
+  const artistsString = artistsIds.join(",");
+
+  const apiResponse = await fetch(
+    `https://api.spotify.com/v1/artists/?ids=${artistsString}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const artistsData = await apiResponse.json();
+
+  return new Response(JSON.stringify(artistsData));
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,35 @@
-const Homepage = () => {
+"use client";
+
+import { useEffect, useState } from "react";
+
+const HomePage = () => {
+  const [artists, setArtists] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    try {
+      fetch("/api/artists")
+        .then((res) => res.json())
+        .then((data) => {
+          setArtists(data.artists);
+          setIsLoading(false);
+        });
+    } catch (error) {
+      setError(error);
+    }
+  }, []);
+  console.log({ artists });
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading artists</p>;
+
   return (
     <div>
-      <h1>Homepage</h1>
+      {artists.map((artist) => {
+        return <h1 key={artist.id}>{artist.name}</h1>;
+      })}
     </div>
   );
 };
-export default Homepage;
+
+export default HomePage;
